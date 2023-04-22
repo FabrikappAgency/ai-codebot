9004d8338bfa32cac9a05c62bf3a442d4950a439
Jeremy <jeremy@fabrikapp.fr>
2023-04-22 23:55:17 +0200

    refactor(analyze-code.mjs): add console logs to track progress and improve readability
    feat(perpetual-cli.mjs): add support for replaceCode flag in analyzeCodeCmd function
    fix(perpetual-cli.mjs): fix typo in analyzeCodeCmd function definition

--- commands/analyze-code.mjs
+++ commands/analyze-code.mjs
@@ -6,43 +6,60 @@ import FormData from "form-data";
 import dotenv from "dotenv";
 import openaiPackage from "@tectalic/openai";
 import { chatCompletion, clearMessages } from "./index.mjs";
+import chalk from "chalk";
 
 dotenv.config();
 const openaiClient = openaiPackage.default(process.env.OPENAI_API_KEY);
 
 openai.apiKey = process.env.OPENAI_API_KEY;
 
-async function analyzeCode(path, prompt = "no specific requirements", replaceCode = false) {
+async function analyzeCode(
+  path,
+  prompt = "no specific requirements",
+  replaceCode = false
+) {
   const codeFilePath = `/Users/jeremy/Documents/dev/FabrikappAgency/ai-scripts/${path}`;
   const dataFilePath =
     "/Users/jeremy/Documents/dev/FabrikappAgency/ai-scripts/data/data.json";
 
   await clearMessages(dataFilePath);
+  console.log(chalk.green(`Starting analysis of ${path}`));
 
   const code = await getFileContent(codeFilePath);
+  
+  
   const context = `You are the best software developer. You are a specialist. But you can only write code. Analyze this code and improve it. Your reply must contain the complete code and nothing else. Here is the code:${code}. You can update the code to implement the following requirements:${prompt}`;
 
   const summary = await chatCompletion(
     "Return a summary of the changes you've made in bullet point with return char.",
     context
   );
-  const analyze = await chatCompletion(
-    "Return a sentence giving your a summary of your improvments.."
-  );
 
-  console.log("summary", summary);
-  console.log("analyze", analyze);
+  // console.log("summary", summary);
+  console.log(chalk.green(`Changes : ${summary}`));
 
-  const updatedCode = await chatCompletion(
-    "Apply this improvments to the code and return the improved code. You don't need to use enclosing quote or code template, just raw code. Your reply must contains the code and nothing else. Please do not add any mdx style annotation, just raw unformatted code."
-  );
-  const formattedCode = clearCodeOutput(updatedCode);
-  console.log("updatedCode", updatedCode);
-  if (formattedCode !== "" && replaceCode === true) {
-    await writeFileContent(codeFilePath, formattedCode).catch((err) => {
-      console.log("errorwriteFileContent", err);
-    });
+  if (replaceCode === true) {
+    const updatedCode = await chatCompletion(
+      "Apply this improvments to the code and return the improved code. You don't need to use enclosing quote or code template, just raw code. Your reply must contains the code and nothing else. Please do not add any mdx style annotation, just raw unformatted code."
+    );
+    const formattedCode = clearCodeOutput(updatedCode);
+    console.log("updatedCode", updatedCode);
+    if (formattedCode !== "" && replaceCode === true) {
+      await writeFileContent(codeFilePath, formattedCode).catch((err) => {
+        console.log("errorwriteFileContent", err);
+      });
+    }
   }
+
+
+
+  const analyze = await chatCompletion(
+    "Return a sentence giving your a summary of your improvments."
+  );
+  console.log(chalk.green(`Summary : ${analyze}`));
+
+  // console.log("analyze", analyze);
+
   return summary;
 }
 
--- perpetual-cli.mjs
+++ perpetual-cli.mjs
@@ -12,12 +12,12 @@ async function fetchData(query) {
   console.log(chalk.green(`Fetched data for query: ${query}`));
   console.log(result);
 }
-async function analyzeCodeCmd(path, prompt = "") {
+async function analyzeCodeCmd(path, prompt = "", replaceCode = false) {) {
   const spinner = ora(
     `Analyze code for page ${path} with prompt ${prompt}`
   ).start();
 
-  const result = await analyzeCode(path, prompt).catch((err) => {
+  const result = await analyzeCode(path, prompt, replaceCode).catch((err) => {
     spinner.stop();
     return;
   });
@@ -50,6 +50,14 @@ async function askQuestion() {
       askQuestion();
     } else if (answer.toLowerCase().startsWith("code")) {
       await analyzeCodeCmd("commands/child_process.mjs");
+      askQuestion();
+    } else if (answer.toLowerCase().startsWith("codefix")) {
+      const words = answer.split(" ");
+      words.shift(); // remove the first word
+      const path = words[0];
+      words.shift(); // remove the first word
+      const prompt = words.join(" ");
+      await analyzeCodeCmd(path, prompt, true);
       askQuestion();
     } else if (answer.toLowerCase().startsWith("analyzecode")) {
       const words = answer.split(" ");
