b720095e57cbd1407b24cc726e7386979eede8e0
Jeremy <jeremy@fabrikapp.fr>
2023-04-23 02:40:46 +0200

    refactor(utils.mjs): use try-catch blocks instead of Promise callbacks for file read and write operations feat(utils.mjs): add error handling to clearCodeOutput function

--- utils/utils.mjs
+++ utils/utils.mjs
@@ -5,54 +5,56 @@ import fs from "fs";
  * @param {string} path - The path of the file to write to.
  * @param {string} data - The data to write to the file.
  * @returns {Promise<boolean>} - A promise that resolves to true if the write was successful.
+ * @throws Will throw an error if the write operation fails.
  */
 async function writeFileContent(path, data) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(path, data, { encoding: "utf8" }, (err) => {
-      if (err) {
-        console.error("An error occurred while writing the file:", err);
-        reject(err);
-      } else {
-        console.log("The file was saved!");
-        resolve(true);
-      }
-    });
-  });
+  try {
+    await fs.promises.writeFile(path, data, { encoding: "utf8" });
+    console.log("The file was saved!");
+    return true;
+  } catch (err) {
+    console.error("An error occurred while writing the file:", err);
+    throw err;
+  }
 }
 
 /**
  * Reads data from a file asynchronously.
  * @param {string} path - The path of the file to read.
  * @returns {Promise<string>} - A promise that resolves to the data read from the file.
+ * @throws Will throw an error if the read operation fails.
  */
 async function getFileContent(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.error("An error occurred while reading the file:", err);
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
+  try {
+    const data = await fs.promises.readFile(path, "utf8");
+    return data;
+  } catch (err) {
+    console.error("An error occurred while reading the file:", err);
+    throw err;
+  }
 }
 
 /**
  * Removes code output from a string.
  * @param {string} code - The string to remove code output from.
  * @returns {string} - The string with code output removed.
+ * @throws Will throw an error if the regular expression fails to match.
  */
 function clearCodeOutput(code) {
-  const regex = /```(?<word>\w+)?\n(?<content>[\s\S]*?)\n```/g;
+  try {
+    const regex = /```(?<word>\w+)?\n(?<content>[\s\S]*?)\n```/g;
 
-  let result = "";
-  let match;
-  while ((match = regex.exec(code)) !== null) {
-    const content = match.groups.content;
-    result += content;
+    let result = "";
+    let match;
+    while ((match = regex.exec(code)) !== null) {
+      const content = match.groups.content;
+      result += content;
+    }
+    return result;
+  } catch (err) {
+    console.error("An error occurred while clearing the code output:", err);
+    throw err;
   }
-  return result;
 }
 
 export { clearCodeOutput, getFileContent, writeFileContent };
