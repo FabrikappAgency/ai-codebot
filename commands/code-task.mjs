4be3705ffbddc1d5e3edbb0ba7671791130922bf
Jeremy <jeremy@fabrikapp.fr>
2023-04-23 05:03:02 +0200

    refactor(code-task.mjs): extract contextFilePath and dataFilePath to environment variables feat(code-task.mjs): add clearData function to clear messages from dataFilePath feat(code-task.mjs): add analyzeSummary function to analyze the validity of JSON with 'command' key feat(code-task.mjs): modify codeTask function to use clearData and analyzeSummary functions and return formatted analyze result if invalid JSON is detected

--- commands/code-task.mjs
+++ commands/code-task.mjs
@@ -1,38 +1,56 @@
 import fs from "fs";
-import openai from "openai";
 import axios from "axios";
 import FormData from "form-data";
 import dotenv from "dotenv";
 import openaiPackage from "@tectalic/openai";
-import { chatCompletion, clearMessages } from "./index.mjs";
 import chalk from "chalk";
-import { getFileContent } from "../utils/utils.mjs";
+import { getFileContent, clearCodeOutput } from "../utils/utils.mjs";
+import { chatCompletion, clearMessages } from "./index.mjs";
 
 dotenv.config();
 const openaiClient = openaiPackage.default(process.env.OPENAI_API_KEY);
 
-openai.apiKey = process.env.OPENAI_API_KEY;
+const contextFilePath = process.env.CONTEXT_FILE_PATH;
+const dataFilePath = process.env.DATA_FILE_PATH;
+
+async function clearData() {
+  await clearMessages(dataFilePath);
+  console.log("Cleared messages from", dataFilePath);
+}
+
+async function analyzeSummary(summary) {
+  try {
+    const json = JSON.parse(summary);
+    if (json.hasOwnProperty("command")) {
+      console.log(chalk.green("Valid JSON with 'command' key detected"));
+      return true;
+    } else {
+      console.log(chalk.red("JSON does not have 'command' key"));
+      return false;
+    }
+  } catch (err) {
+    console.log(chalk.red("Invalid JSON"));
+    return false;
+  }
+}
 
 async function codeTask(
   prompt = "no specific requirements",
   replaceCode = false
 ) {
-  const contextFilePath = "/Users/jeremy/Documents/dev/FabrikappAgency/ai-scripts/prompts/find_commands.txt";
-  const dataFilePath = "/Users/jeremy/Documents/dev/FabrikappAgency/ai-scripts/data/data.json";
-
-  await clearMessages(dataFilePath);
-  console.log("clearMessages", contextFilePath);
-
+  await clearData();
   const context = await getFileContent(contextFilePath);
-console.log("context", context);
-  const summary = await chatCompletion(prompt, context);
-   
-  console.log(chalk.green(`Changes : ${summary}`));
-
-  return summary;
+  const analyze = await chatCompletion(prompt, context);
+  console.log(chalk.green(`Analyze : ${analyze}`));
+  const isAnalyzeValid = await analyzeSummary(analyze);
+  if (isAnalyzeValid) {
+    return analyze;
+  } else {
+    const formattedAnalyze = clearCodeOutput(analyze);
+    //   console.log(chalk.green(`Changes : ${summary}`));
+    const isValid = await analyzeSummary(formattedAnalyze);
+    return isValid ? formattedAnalyze : "";
+  }
 }
-
-
-
 
 export { codeTask };
